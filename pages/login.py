7f4c0f44d0fbdad6a76bcb765301f9898e9c0d94
Ismael Ventura <ismael.ventura@ies2.com.br>
2021-01-24 16:00:51 -0300

    Adicionando Page Object do login.

---
+++ pages/login.py
@@ -0,0 +1,13 @@
+class Login:
+    def __init__(self, driver):
+        self._driver = driver
+        self._email_field = self._driver.find_element_by_id("email")
+        self._passwd_field = self._driver.find_element_by_id("passwd")
+        self._sign_in = self._driver.find_element_by_id("SubmitLogin")
+
+    def login(self, email: str, passwd: str) -> None:
+        self._email_field.clear()
+        self._email_field.send_keys(email)
+        self._passwd_field.clear()
+        self._passwd_field.send_keys(passwd)
+        self._sign_in.click()
