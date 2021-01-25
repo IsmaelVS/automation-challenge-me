cbe7c3af5abad8ab1c0ad921cdbd32cbd3be9cc2
Ismael Ventura <ismael.ventura@ies2.com.br>
2021-01-24 21:37:41 -0300

    Criando função para setar valor no select.

--- helpers/helper.py
+++ helpers/helper.py
@@ -1,1 +1,12 @@
 """File for helper in project."""
+from selenium.webdriver.support.ui import Select
+
+
+def select_item(self, id, value):
+    select_field = self.driver.find_element_by_id(id)
+    select = Select(select_field)
+    options = select.options
+
+    for option in options:
+        if option.text.strip() == value:
+            option.click()
